34c55e70f3399c14af6ae4c939e8bcdee97a636b
Kevin Welcher <kevin.welcher@gmail.com>
2023-05-31 13:05:32 -0400

    Alters scrape to use chunking

--- packages/verses-shared/scrape-data/helpers/scrapeLeaves.ts
+++ packages/verses-shared/scrape-data/helpers/scrapeLeaves.ts
@@ -1,25 +1,28 @@
-import { TreeClass, Leaf } from '../../types/Tree'
+import { TreeClass, Leaf, LeafId } from '../../types/Tree'
 import { scrapeVerse } from './scrapeVerse'
 import progress from 'cli-progress'
 
 export async function scrapeLeaves(tree: TreeClass): Promise<Leaf[]> {
+  const chunkSize = 40
+
+  let ids: LeafId[] = []
   let leaves: Leaf[] = []
   let totalLeaves = tree.numLeaves()
-  let count = 0
-
-  const bar = new progress.SingleBar(
-    {
-      etaBuffer: 1000,
-    },
-    progress.Presets.shades_classic
-  )
-  bar.start(totalLeaves, 0)
 
   await tree.forEachLeaf(async (leaf) => {
-    const leafData = await scrapeVerse(leaf.path)
-    leaves.push(leafData)
-    bar.update(count++)
+    ids.push(leaf.path)
   })
+
+  const bar = new progress.SingleBar(progress.Presets.shades_classic)
+  bar.start(ids.length, 0)
+
+  do {
+    let chunk = await Promise.all(
+      ids.splice(0, chunkSize).map((id) => scrapeVerse(id))
+    )
+    leaves = leaves.concat(chunk)
+    bar.update(leaves.length)
+  } while (ids.length > 0)
 
   bar.update(totalLeaves)
   bar.stop()
